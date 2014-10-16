74636e5e723070308c7fec30697ba631e8a0d5be
Skyler Manzanares <skymanzanares@hotmail.com>
2014-10-16 14:11:40 -0600

    Created package and plugin class.

---
+++ src/com/SkyIsland/SecretVillager/SecretVillagerPlugin.java
@@ -0,0 +1,21 @@
+package com.SkyIsland.SecretVillager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SecretVillagerPlugin extends JavaPlugin {
+	
+	@Override
+	public void onLoad() {
+		
+	}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
