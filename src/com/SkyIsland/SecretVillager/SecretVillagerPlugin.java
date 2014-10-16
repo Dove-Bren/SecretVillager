f294e911a28b826f80d821bd21aa49dfc02a0a31
Skyler Manzanares <skymanzanares@hotmail.com>
2014-10-16 14:22:43 -0600

    Added a config loading section to onLoad of main plugin

--- src/com/SkyIsland/SecretVillager/SecretVillagerPlugin.java
+++ src/com/SkyIsland/SecretVillager/SecretVillagerPlugin.java
@@ -1,21 +1,84 @@
 package com.SkyIsland.SecretVillager;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SecretVillagerPlugin extends JavaPlugin {
 	
+	public static SecretVillagerPlugin plugin;
+	
+	private YamlConfiguration config;
+	private YamlConfiguration villagerConfig;
+	
 	@Override
 	public void onLoad() {
+		File configFile, villagerFile;
+		configFile = new File(this.getDataFolder(), "config.yml");
+		if (!configFile.exists()) {
+			getLogger().info("Configuration file for Secret Villager plugin not found in data folder!\n"
+					+ "Making a default one...");
+			config = defaultConfig();
+			try {
+				config.save(configFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+				getLogger().info("Unable to save default config!\nAborting plugin load...");
+				config = null;
+				return;
+			}
+		}
+		else {
+			try {
+				config.load(configFile);
+			} catch (IOException
+					| InvalidConfigurationException e) {
+				e.printStackTrace();
+				getLogger().info("Unable to load config from config.yml!!\nAborting plugin load...");
+				config = null;
+				return;
+			}
+		}
+		
+		//config loaded/was created correctly
+		//do villager config now
+		villagerFile = new File(this.getDataFolder(), "villagers.yml");
+		if (!villagerFile.exists()) {
+			//we don't really have a default. Just make an empty one and have an empty config.
+			try {
+				villagerFile.createNewFile();
+				villagerConfig = new YamlConfiguration();
+			} catch (IOException e) {
+				e.printStackTrace();
+				getLogger().info("Unable to place a new file named 'villagers.yml' in resource folder.\nAborting plugin load...");
+				config = null;
+				villagerConfig = null;
+				return;
+			}
+		}
+		
 		
 	}
 	
 	@Override
 	public void onEnable() {
-		
+		SecretVillagerPlugin.plugin = this;
 	}
 	
 	@Override
 	public void onDisable() {
 		
+	}
+	
+	
+	
+	
+	private YamlConfiguration defaultConfig() {
+		
+		return null;
 	}
 }
